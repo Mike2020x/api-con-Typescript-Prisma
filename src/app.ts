19da15e896207ecf5c26c537514122d5a0ed46c5
Mike2020x <62442141+Mike2020x@users.noreply.github.com>
2023-06-08 08:30:45 -0500

    feat: : add folders and prisma schema

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
 import express from "express";
-const app = express()
+import configExpress from "./config/express";
 
+const app = express()
 const PORT = process.env.PORT || 8080
+
+configExpress(app)
+
 app.listen(PORT, () => {
   console.log(`App running in http://localhost:${PORT}`);
-})
-
-app.get("/", (req, res) => {
-  res.send("Hello")
 })
